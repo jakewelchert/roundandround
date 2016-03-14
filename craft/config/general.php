db8065f4fbb74c09a9e9c175b4e0c667377a7157
Jake WelchertE <jakewelchert@gmail.com>
2016-03-14 01:18:51 -0500

    updates to servers

--- craft/config/general.php
+++ craft/config/general.php
@@ -8,5 +8,22 @@
  */
 
 return array(
+  'localhost' => array(
+    'devMode' => true,
+    'siteUrl' => 'http://localhost:8888/',
+    'environmentVariables' => array(
+      'basePath' => '/users/eugene/Sites/craft-project/html/',
+      'baseUrl'  => 'http://localhost:8888/',
+    ),
+    'testToEmailAddress' => 'dev@email.com',
+  ),
 
+  // Use IP address of your droplet below
+  '198.211.114.192/' => array(
+    'siteUrl' => 'http://198.211.114.192/',
+    'environmentVariables' => array(
+      'basePath' => '/var/www/html/',
+      'baseUrl'  => 'http://198.211.114.192/',
+    )
+  )
 );
